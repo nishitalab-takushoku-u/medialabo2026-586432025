10f2f0b122b70f469377238109bdbd095a1c1689
G58643 <vBh(98x2BAu4>
2026-05-14 23:57:17 +0900

    5/14

--- kazuate.js
+++ kazuate.js
@@ -4,26 +4,41 @@
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
-// 入力回数（予想回数）
 let kaisu = 0;
-
-// そのほか，必要に応じて変数を宣言してもよい
-
-// ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  // ここから: 予想回数を1増やして，span#kaisu 要素のテキストを更新
-
-  // ここまで: 予想回数を1増やして，span#kaisu 要素のテキストを更新
-  
-  // ここから: テキストボックスに指定された数値を yoso に代入する
+  kaisu=kaisu+1;
   let yoso;
+
+  let AK=document.querySelector('p#kaisu');
+  AK.textContent=kaisu;
+  
+  let AL=document.querySelector('p#left');
+  yoso=Number(AL);
+
+  let En=document.querySelector('p#result');
+
+  if(kaisu>2){
+    En.textContent='まちがい。残念でした。答えは'+kotae+'です。';
+  }
+  else if(yoso===kotae){
+    En.textContent='正解です。おめでとう！';
+  }
+  else if(yoso<kotae){
+    En.textContent='まちがい。答えはもっと大きいですよ';
+  }
+  else if(yoso>kotae){
+    En.textContent='まちがい。答えはもっと小さいですよ';
+  }
+}
+let b= document.querySelector('button#kettei');
+b.addEventListener('click',hantei);
+  
   // ここまで: テキストボックスに指定された数値を yoso に代入する
   
   // ここから: 正解判定する
   // 　　　　  正解/不正解のときのメッセージを表示する
 
   // ここまで: 正解判定する
-}
 
 // ここから: ボタンを押した時のイベントハンドラとして hantei を登録
 
