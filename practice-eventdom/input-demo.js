194621629a0c0f686bb36ba96898edaab05cf4aa
G58643 <vBh(98x2BAu4>
2026-05-14 16:14:50 +0900

    5/14

--- practice-eventdom/input-demo.js
+++ practice-eventdom/input-demo.js
@@ -1,5 +1,9 @@
-function greeting() {
-    console.log('こんにちは');
-  } 
-p = document.querySelector('button#print');
-p.addEventListener('click', greeting);
+function greeting2() {
+ let i = document.querySelector('input[name="shimei"]');
+  let shimei = i.value;
+  let asisatu = 'こんにちは、'+shimei+'さん';
+  let p=document.querySelector('p#message');
+  p.textContent=asisatu;
+}
+let b= document.querySelector('button#print');
+b.addEventListener('click',greeting2);
