45ac76948ec43b2acb1f9c633296ba359084f9b5
G58643 <vBh(98x2BAu4>
2026-05-21 17:47:50 +0900

    5/21

--- weather.js
+++ weather.js
@@ -20,49 +20,53 @@ function printDom(data) {
     let b=document.querySelector('body');
     b.insertAdjacentElement('beforeend',d);
 
-    let u=document.createElement('ul');/*
+
+
+    let u=document.createElement('ul');
     let l;
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="1件目の検索結果";
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="緯度："+data.coord.lat;
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="経度:"+data.coord.lon;
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="天気:"+data.weather[0].description;
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="最低気温:"+data.main.temp_min;
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="最高気温:"+data.main.temp_max;
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="湿度:"+data.main.humidity;
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="風速:"+data.wind.speed;
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="風向:"+data.wind.deg;
     u.insertAdjacentElement('beforeend', l);
 
-    l=document.createElement('l');
+    l=document.createElement('li');
     l.textContent="都市名:"+data.name;
     u.insertAdjacentElement('beforeend', l);
-    */
+
+    d.insertAdjacentElement('beforeend', u);
+  
    }
 
 
